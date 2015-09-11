8207ca729605f4a7ff725864ff36c6686edd3396
JuanPi Carbajal <juanpi@dwengo.org>
2015-09-12 01:46:46 +0200

    Full example in readme

--- examples/SingleButton/Singlebutton.ino
+++ examples/SingleButton/Singlebutton.ino
@@ -1,35 +1,10 @@
-EventManager
-================
-
-A library for attaching *Callbacks* to *Event Publishers* for the Dwenguino/Arduino platform.
-
-Introduction
-------------
-It is quite comon to associate a function call to an event.
-For example, if pin **X** of the board goes **UP** one would like to turn a **LED** on.
-To do this we need an object (the Manager) that knows that function `ledOn` should be called
-when X goes up. Something like:
-
-```
-manager.bind (ledOn, X_up)
-```
-
-Here `manager` is the object that knows the relation betwen the *callback*[[1]](#callback) `ledOn`
-and the *event publisher* `X_up`.
-
-
-### Concrete Example
-
-The example above can be made concrete. Lets say we waiting for a LOW in pin 2 (which
-was correctly configured). The code looks like:
-```c++
 #include <EventManager.h>
 
 class Publisher: public EventPublisher
 {
   uint8_t pin;
   unsigned int sinceLast;
-
+  
   public:
     // This is the constructor. It is called
     // whenever we create an instance of this class.
@@ -58,16 +33,16 @@ const int ledPin = 13;
 // All callbacks should receive a pointer to Event.
 // Here we do not use it because this is a silly example
 void writeHIGHtoPin(const Event * ev)
-{
+{ 
   static bool state = false; // just to remember the button state
   if (state)
   {
-    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin, LOW); 
     state = false;
   }
   else
   {
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin, HIGH); 
     state = true;
   }
 }
@@ -93,16 +68,4 @@ void loop()
 {
   dwenguinoManager.update();
 }
-```
 
-See the `examples` folder for this and other examples of use.
-
-Related libraries
------------------
-* [Bounce](http://playground.arduino.cc/Code/Bounce)
-
-* [Messenger](http://playground.arduino.cc/Code/CmdMessenger)
-
-
-<a name="callback">[1]</a>: Callback is a fancy name for functions that are called
-when something happens.
